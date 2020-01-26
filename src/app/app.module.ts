342f0cfaae814a79b7e35a90e6159b2a2689aa4f
Ali Padron <alijesus29@gmail.com>
2020-01-26 12:36:51 -0400

    Heroe Tarjeta component with Input properties.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SearchResultsComponent } from './components/search-results/search-resul
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
