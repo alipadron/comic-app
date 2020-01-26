1facb5be3b165f16b475f7c0af9335c751974c4e
Ali Padron <alijesus29@gmail.com>
2020-01-25 21:29:53 -0400

    Heroe detail component added and router links with parameters are working.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HeroesService, Heroe } from "src/app/services/heroes.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-heroes",
@@ -7,11 +8,14 @@ import { HeroesService, Heroe } from "src/app/services/heroes.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
-  constructor(private heroesService: HeroesService) {}
+  constructor(private heroesService: HeroesService, private router: Router) {}
 
   ngOnInit() {
     this.heroes = this.heroesService.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
+  }
+
+  verHeroe(index: number) {
+    this.router.navigate(["/heroe", index]);
   }
 }
-
